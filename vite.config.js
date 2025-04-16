7f06abbf1122829ce185e30f1fbb0c35342c6fb4
Mike Stephens <hello@craftcmsdeveloper.co.uk>
2025-04-16 08:52:42 +0100

    Added MPA support and swap out static sitemap with auto generator

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import tailwindcss from "@tailwindcss/vite";
+import handlebars from 'vite-plugin-handlebars';
+import Sitemap from 'vite-plugin-sitemap'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            about: resolve(__dirname, 'about.html'),
+          },
+        },
+      },
+    plugins: [
+        tailwindcss(),
+        handlebars({
+            partialDirectory: resolve(__dirname, 'partials'),
+        }),
+        Sitemap({ hostname: 'https://craftcmsdeveloper.co.uk' }),
+    ],
 })
